56fbaf69910825fd86f55cb4a030248378c70e7d
Himanshi Dafouty <116953498+HimanshiDafouty@users.noreply.github.com>
2025-02-24 22:57:28 +0530

    cra & tailwindcss

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" className = "text-3xl font-bold">
+      Netlfix-GPT
+
+     
+    </div>
+  );
+}
+
+export default App;
